d7861f551936c3c826203a9f7792293906820a4a
jun-hf <fong.junh@gmail.com>
2022-10-12 12:09:47 +0100

    Test[Model entry]

--- tests/journal/test_models.py
+++ tests/journal/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from journal.models import Log
+from journal.models import Log, Entry
 
 @pytest.mark.django_db
 def test_log_model():
@@ -10,3 +10,17 @@ def test_log_model():
     assert log.content == "I am very happy today"
     assert str(log) == log.content
     assert log.date_added
+
+@pytest.mark.django_db
+def test_entry_model():
+    log = Log(content="I like to code")
+    log.save() 
+
+    entry = Entry(topic=log, content="I like to code in python")
+    entry.save() 
+
+    assert entry.topic == log
+    assert entry.content == "I like to code in python"
+    assert entry.date_added
+    assert str(entry) == "I like to code in python"
+    
